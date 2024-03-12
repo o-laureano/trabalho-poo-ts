744ccbfb8243d17dfbe086e27b9dd7cb550743e8
Henrique Laureano de Souza <henrique.souza@grupointegrado.br>
2024-03-12 20:26:54 -0300

    Atualizando projeto usando Static

--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,21 @@
 class Pessoa {
-  private _nome: string; // NÃO PODE RECEBER NENHUM VALOR NUMÉRICO!
+  private _nome: string;
   private _idade: number;
   private _email: string;
 
   constructor(nome: string, idade: number, email: string) {
+    this.validarNome(nome);
     this._nome = nome;
     this._idade = idade;
     this._email = email;
   }
 
-  validarNome(nome: string) {
-    //  if (conterAlgumNumero) {
-    //     throw new Error("Erro: O nome não pode conter caracteres numéricos.")
-    //  }
-    return nome;
+  private validarNome(nome: string): void {
+    if (/\d/.test(nome)) {
+      throw new Error(
+        `Erro: O nome ${nome} contem caracteres numéricos, substitua o número por letra!`
+      );
+    }
   }
 
   get nome(): string {
@@ -28,78 +30,96 @@ class Pessoa {
     return this._email;
   }
 
+  set nome(novoNome: string) {
+    this._nome = novoNome;
+  }
+
+  set idade(novaIdade: number) {
+    this._idade = novaIdade;
+  }
+
+  set email(novoEmail: string) {
+    this._email = novoEmail;
+  }
+
   imprimirDados(): string {
     return `Nome: ${this._nome}, Idade: ${this._idade}, Email: ${this._email}`;
   }
-
-  adicionar() {}
-
-  listar() {}
-
-  buscarPeloNome() {}
-
-  atualizar() {}
-
-  deletar() {}
 }
 
 class BancoDeDados {
-  private listaDePessoas: Array<Pessoa> = [];
+  private static listaDePessoas: Array<Pessoa> = [];
 
-  constructor() {}
-
-  adicionarAoBanco(pessoa: Pessoa): void {
-     this.listaDePessoas.push(pessoa);
-     console.log(`Pessoa ${pessoa.nome} adicionada com sucesso.`);
+  static adicionarAoBanco(pessoa: Pessoa): void {
+    this.listaDePessoas.push(pessoa);
+    console.log(`Pessoa ${pessoa.nome} adicionada com sucesso.`);
   }
 
-  listar() {
+  static listar(): void {
     console.log("Lista de Pessoas:");
-    this.listaDePessoas.forEach((pessoa) => console.log(pessoa));
+    this.listaDePessoas.forEach((pessoa) =>
+      console.log(pessoa.imprimirDados())
+    );
   }
 
-  buscarPeloNome() {}
-
-  atualizar() {}
-
-  deletar() {}
+  static 
 }
 
 class Menu {
-  private _bancoDeDados: BancoDeDados;
-
-  constructor(bancoDeDados: BancoDeDados) {
-    this._bancoDeDados = bancoDeDados;
+  constructor() {
   }
 
-  adicionarMenu(pessoa: Pessoa) {
-    this._bancoDeDados.adicionarAoBanco(pessoa);
+  static adicionarMenu(pessoa: Pessoa): void {
+    BancoDeDados.adicionarAoBanco(pessoa);
   }
 
-  listar() {}
+  static alterarNome(pessoa: Pessoa, novoNome: string): void {
+    pessoa.nome = novoNome;
+    console.log(`Nome da pessoa alterado para: ${pessoa.nome}`);
+  }
 
-  buscarPeloNome() {}
+  static alterarIdade(pessoa: Pessoa, novaIdade: number): void {
+    pessoa.idade = novaIdade;
+    console.log(`Idade da pessoa alterado para: ${pessoa.idade}`);
+  }
 
-  atualizar() {}
+  static alterarEmail(pessoa: Pessoa, novoEmail: string): void {
+    pessoa.email = novoEmail;
+    console.log(`Email da pessoa alterado para: ${pessoa.email}`);
+  }
 
-  deletar() {}
+  static listarMenu() {
+    BancoDeDados.listar();
+  }
+
+  static deletarMenu() {
+
+  }
+
+  static buscarPeloNome() {
+
+  }
+
 }
 
-const bancoDeDados = new BancoDeDados();
-const menu = new Menu(bancoDeDados);
-
-console.log(bancoDeDados);
-
-const pessoa1 = new Pessoa("Raimundo1", 55, "raimundo@gmail.com");
+const pessoa1 = new Pessoa("Raimundo", 55, "raimundo@gmail.com");
 const pessoa2 = new Pessoa("Lucas", 24, "lucas@gmail.com");
 const pessoa3 = new Pessoa("Bianca", 18, "bianca@gmail.com");
 const pessoa4 = new Pessoa("Júlia", 13, "julia@gmail.com");
 const pessoa5 = new Pessoa("João", 35, "joao@gmail.com");
 
-menu.adicionarMenu(pessoa1);
-menu.adicionarMenu(pessoa2);
-menu.adicionarMenu(pessoa3);
-menu.adicionarMenu(pessoa4);
-menu.adicionarMenu(pessoa5);
+console.log(pessoa1.nome); // Uso do GET
 
-console.log(bancoDeDados);
+Menu.adicionarMenu(pessoa1);
+Menu.adicionarMenu(pessoa2);
+Menu.adicionarMenu(pessoa3);
+Menu.adicionarMenu(pessoa4);
+Menu.adicionarMenu(pessoa5);
+
+Menu.alterarNome(pessoa1, "Marcelo");
+Menu.alterarIdade(pessoa2, 100);
+Menu.alterarEmail(pessoa3, "email@alterado.com");
+
+// BancoDeDados.listar();
+
+Menu.listarMenu()
